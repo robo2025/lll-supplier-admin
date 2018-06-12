71c7c38d12df1a341407cb7c7703f44a5f4da962
lyin <ling95613@163.com>
2018-06-12 17:37:50 +0800

    完善整数校验

--- program/src/components/CustomeForm/NewGoodForm.js
+++ program/src/components/CustomeForm/NewGoodForm.js
@@ -275,7 +275,9 @@ export default class NewGoodForm extends Component {
               {getFieldDecorator('min_buy', {
                 rules: [{
                   required: true,
-                  type: 'string',
+                  message: '采购数量不能为空',
+                }, {
+                  pattern: /^(\d+)$/,
                   message: '最低采购量必须是整数',
                 }],
                 initialValue: data.min_buy >> 0,
@@ -326,12 +328,12 @@ export default class NewGoodForm extends Component {
                   required: true,
                   message: '请输入库存数量',
                 }, {
-                  type: 'number',
+                  pattern: /^(\d+)$/,
                   message: '库存数量必须是整数',
                 }],
                 initialValue: data.stock,
               })(
-                <Input />
+                <Input type="number" />
               )}
             </FormItem>
           </Form>
