87b8c40745effbb36c5e5054017903ead46c08be
paul <420325873@qq.com>
2018-07-16 16:31:31 +0800

    供应商权限问题

--- program/src/models/user.js
+++ program/src/models/user.js
@@ -1,7 +1,20 @@
 import Cookies from 'js-cookie';
-import { queryCurrent, getUserInfo, registerUser, getSMSCode, getSupplierInfo } from '../services/user';
+import {
+  queryCurrent,
+  getUserInfo,
+  registerUser,
+  getSMSCode,
+  getSupplierInfo,
+} from '../services/user';
 import { setAuthority } from '../utils/authority';
-import { SUCCESS_STATUS } from '../constant/config.js';
+import {
+  SUCCESS_STATUS,
+  USER_INFO,
+  HOME_PAGE,
+  LOGIN_URL,
+  VERIFY_PAGE,
+} from '../constant/config.js';
+import { queryString } from '../utils/tools';
 
 export default {
   namespace: 'user',
@@ -22,8 +35,13 @@ export default {
       });
       const response = yield call(getUserInfo);
       if (response.rescode >> 0 === SUCCESS_STATUS) {
-        if (typeof success === 'function') { success(response); }
-      } else if (typeof error === 'function') { error(response); return; }
+        if (typeof success === 'function') {
+          success(response);
+        }
+      } else if (typeof error === 'function') {
+        error(response);
+        return;
+      }
 
       yield put({
         type: 'save',
@@ -45,24 +63,82 @@ export default {
     *register({ data, success, error }, { call }) {
       const response = yield call(registerUser, { data });
       if (response.rescode >> 0 === SUCCESS_STATUS) {
-        if (typeof success === 'function') { success(response); }
-      } else if (typeof error === 'function') { error(response); }
+        if (typeof success === 'function') {
+          success(response);
+        }
+      } else if (typeof error === 'function') {
+        error(response);
+      }
     },
     *fectchSMS({ mobile, success, error }, { call }) {
       const response = yield call(getSMSCode, { mobile });
       if (response.rescode >> 0 === SUCCESS_STATUS) {
-        if (typeof success === 'function') { success(response); }
-      } else if (typeof error === 'function') { error(response); }
+        if (typeof success === 'function') {
+          success(response);
+        }
+      } else if (typeof error === 'function') {
+        error(response);
+      }
     },
     *fetchSupplierInfo({ supplierId, success, error }, { call, put }) {
       const response = yield call(getSupplierInfo, { supplierId });
       if (response.rescode >> 0 === SUCCESS_STATUS) {
-        if (typeof success === 'function') { success(response); }
-      } else if (typeof error === 'function') { error(response); return; }
+        if (typeof success === 'function') {
+          success(response);
+        }
+      } else if (typeof error === 'function') {
+        error(response);
+        return;
+      }
       yield put({
         type: 'saveSupplier',
         payload: response.data,
       });
+    },
+    *verify(_, { call, put }) {
+      const { href } = window.location;
+      const paramas = queryString.parse(href);
+      const token = Cookies.get('access_token');
+      if (token) {
+        const userinfo = yield call(getUserInfo);
+        const {
+          data: { id },
+        } = userinfo;
+        const response = yield call(getSupplierInfo, { supplierId: id });
+        if (response.data.profile.audit_status !== 1) {
+          setAuthority('1'); // 1为账号未通过审核的权限
+          location.href = `${USER_INFO}`;
+        } else {
+          setAuthority('2'); // 2为账号通过审核的权限
+          window.location.href = HOME_PAGE;
+        }
+      } else if (paramas.access_token) {
+        /* 判断url是否有access_token,如果有则将其存储到cookie */
+        const accessToken = paramas.access_token.split('#/')[0];
+        if (location.host.indexOf('robo2025') !== -1) {
+          Cookies.set('access_token', accessToken, {
+            expires: 7,
+            path: '/',
+            domain: '.robo2025.com',
+          });
+        } else {
+          Cookies.set('access_token', accessToken);
+        }
+        const userinfo = yield call(getUserInfo);
+        const {
+          data: { id },
+        } = userinfo;
+        const response = yield call(getSupplierInfo, { supplierId: id });
+        if (response.data.profile.audit_status !== 1) {
+          setAuthority('1'); // 1为账号未通过审核的权限
+          location.href = `${USER_INFO}`;
+        } else {
+          setAuthority('2'); // 2为账号通过审核的权限
+          window.location.href = HOME_PAGE;
+        }
+      } else {
+        window.location.href = `${LOGIN_URL}?next=${VERIFY_PAGE}&from=supplier`;
+      }
     },
   },
 
